8af42be3821bbd67907428e54a97c8b29f99de93
Paulo Cesar dos Santos <paulo.santoss312@gmail.com>
2026-08-30 16:25:47 -0300

    add filmes, series e titulo

--- src/Principal.java
+++ src/Principal.java
@@ -1,18 +1,28 @@
-public class Principal {
-    public static void main(String[] args) {
-        Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
-        meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(8);
-        meuFilme.avalia(5);
-        meuFilme.avalia(10);
-        System.out.println("total de avaliações:"+meuFilme.getTotalDeAvaliacoes());
-        System.out.println(meuFilme.pegaMedia());
+public class Principal {
+    public static void main ( String[] args ) {
+        Filme meuFilme = new Filme ( );
+        meuFilme.setNome ( "O poderoso chefão" );
+        meuFilme.setAnoDeLancamento ( 1970 );
+        meuFilme.setDuracaoEmMinutos ( 180 );
+
+        meuFilme.exibeFichaTecnica ( );
+        meuFilme.avalia ( 8 );
+        meuFilme.avalia ( 5 );
+        meuFilme.avalia ( 10 );
+        System.out.println ( "total de avaliações:" + meuFilme.getTotalDeAvaliacoes ( ) );
+        System.out.println ( meuFilme.pegaMedia ( ) );
         //meuFilme.somaDasAvaliacoes=10;
         //meuFilme.somaDasAvaliações=1;
         //System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie ( );
+        lost.setNome ( "lost" );
+        lost.setAnoDeLancamento ( 2000 );
+        lost.exibeFichaTecnica ( );
+        lost.setTemporadas ( 10 );
+        lost.setEpisodiosPorTemporada ( 10 );
     }
 }
