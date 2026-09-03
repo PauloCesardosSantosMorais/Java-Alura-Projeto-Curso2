bce52247d2e08aae1911835f0e7de2f5cbbde0b5
Paulo Cesar dos Santos <paulo.santoss312@gmail.com>
2026-09-03 12:02:55 -0300

    adiciona nova classe

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,3 +1,5 @@
+package principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
